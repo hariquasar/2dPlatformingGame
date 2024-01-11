3b05ec9a9dc2c83bfe08b60d7ca49ebdc776057f
hariquasar <138948301+hariquasar@users.noreply.github.com>
2024-01-11 17:20:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,33 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+class Player {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0,
+        }
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 100, 100)
+    }
+
+    update(){
+        this.draw()
+        this.position.y++
+    }
+
+}
+
+const player = new Player()
+const player2 = new Player()
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    player2.update()
+}
+
+animate()
